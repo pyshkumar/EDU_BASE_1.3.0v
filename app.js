d8936ed1e0d7db395b4bf24e653e15767cd5a09c
Piyush Kumar <74560941+pyshkumar@users.noreply.github.com>
2022-12-05 17:00:57 +0530

    removed confidential credentails

--- app.js
+++ app.js
@@ -210,8 +210,8 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    callbackURL: ,
+    userProfileURL: 
   },
   function(accessToken, refreshToken, profile, cb) {
     person.googleId = profile.id;
@@ -449,8 +449,8 @@ function generateOTP() {
 const smptTransport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'edubasejiit@gmail.com',
-    pass: 'kegrcktkasiwdjgx'
+    user: ,
+    pass: 
   }
 });
 
